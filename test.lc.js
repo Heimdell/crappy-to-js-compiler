73572f942e23f24aef57e5ae3605ef716c888749
Андреев Кирилл <hindmost.one@gmail.com>
2016-06-13 23:20:29 +0400

    Added compilation results

---
+++ test.lc.js
@@ -0,0 +1,34 @@
+var it =  (((() => {
+               var  x = (require) ("http")
+               function cata (map) { return (
+                   (fold) => ((function  itself (layer)  { return (
+                                  (fold) ((map) (itself) (layer))
+                              )}))
+               )}
+               function ana (map) { return (
+                   (unfold) => ((function  itself (seed)  { return (
+                                    (map) (itself) ((unfold) (seed))
+                                )}))
+               )}
+               function pair (x) { return (
+                   (y) => (f) => (f) (x) (y)
+               )}
+               function _49_st (p) { return (
+                   (p) ((x) => (y) => (x))
+               )}
+               function _50_nd (p) { return (
+                   (p) ((x) => (y) => (y))
+               )}
+               function left (x) { return (
+                   (f) => (g) => (f) (x)
+               )}
+               function right (x) { return (
+                   (f) => (g) => (g) (x)
+               )}
+               function either (f) { return (
+                   (g) => (e) => (e) (f) (g)
+               )}
+               var  plus = (x => y => x + y)
+               return (x.createServer)
+           }) ()))
+console.log (it)
